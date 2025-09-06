56b6db281b2b04d0d766ab6d5218b0aac59c8d7a
Vaibhav Saini <145651754+VaibhavEra@users.noreply.github.com>
2025-09-06 10:44:31 +0000

    refactor(api): changed response body in backend for movieDetails

--- ui-refactor/src/data/movieDetails.js
+++ ui-refactor/src/data/movieDetails.js
@@ -3,10 +3,19 @@ export const movieData = {
     id: 693134,
     title: "Dune: Part Two",
     poster_url:
-      "https://image.tmdb.org/t/p/original/6izwz7rsy95ARzTR3poZ8H6c5pp.jpg",
+      "https://image.tmdb.org/t/p/w500/6izwz7rsy95ARzTR3poZ8H6c5pp.jpg",
     backdrop_url:
       "https://image.tmdb.org/t/p/w780/o869RihWTdTyBcEZBjz0izvEsVf.jpg",
-    genres: ["Science Fiction", "Adventure"],
+    genres: [
+      {
+        id: 878,
+        name: "Science Fiction",
+      },
+      {
+        id: 12,
+        name: "Adventure",
+      },
+    ],
     original_title: "Dune: Part Two",
     primary_language: "en",
     synopsis:
@@ -17,12 +26,35 @@ export const movieData = {
     },
     runtime: 167,
     oneliner: "Long live the fighters.",
+    belongs_to_collection: {
+      id: 726871,
+      name: "Dune Collection",
+    },
   },
   additional: {
-    production_companies: ["Legendary Pictures"],
-    origin_country: ["United States of America"],
-    spoken_languages: ["English"],
+    production_companies: [
+      {
+        id: 923,
+        name: "Legendary Pictures",
+        origin_country: "US",
+      },
+    ],
+    origin_country: ["US"],
+    production_countries: [
+      {
+        iso_3166_1: "US",
+        name: "United States of America",
+      },
+    ],
+    spoken_languages: [
+      {
+        english_name: "English",
+        iso_639_1: "en",
+        name: "English",
+      },
+    ],
     primary_language: "en",
+    status: "Released",
   },
   financial: {
     budget: 190000000,
@@ -31,5 +63,6 @@ export const movieData = {
   platforms: {
     imdb_id: "tt15239678",
     tmdb_id: 693134,
+    homepage: "https://www.dunemovie.com",
   },
 };
